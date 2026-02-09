ad3de3dae721a23d676b165023e9c749876e08b0
harms <yao.hgk@obmontessori.edu.ph>
2026-02-09 09:03:57 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyscript import display, document
+
+def account_creation(e):
+    document.getElementById('output').innerHTML = " "
+
+    username = document.getElementById('username').value
+    password = document.getElementById('pass').value
+
+    if len(username) < 7: 
+        if password.isdigit() and password.isalpha():
+            display(f"you are account has been created", target="output")
+
+        elif len(password) >= 10 and password.isdigit() or password.isalpha():
+            display(f"your password is too weak. please include both numbers and letters", target="output")
+        else:
+            display(f"your password is too short. please make it at least 10 characters long", target="output")
+    else: 
+        display(f"your account has been created successfully!", target="output")
